b595ad252518782a650d28dda0c54e7c1508ef20
LyleMi <1278919492@qq.com>
2016-12-18 15:01:22 +0800

    add

--- pathdiff.py
+++ pathdiff.py
@@ -1,4 +1,16 @@
 import os
+from hashlib import sha256
+
+
+def sha2(s):
+    return sha256(str(s)).hexdigest()
+
+
+def filesha(path):
+    tmp = open(path, 'r')
+    s = sha2(tmp.read())
+    tmp.close()
+    return s
 
 
 def traverseDir(path):
@@ -8,7 +20,7 @@ def traverseDir(path):
             print 'dir', os.path.join(path, i)
             traverseDir(os.path.join(path, i))
         elif os.path.isfile(i):
-            print 'file', i
+            print 'file', i, "sha", filesha(i)
 
 if __name__ == '__main__':
     traverseDir(os.getcwd())
