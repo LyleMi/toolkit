3262f3faa25c8ed7e444d68fe468909504bd4286
LyleMi <1278919492@qq.com>
2017-02-04 16:17:58 +0800

    change

--- hashCrack.py
+++ hashCrack.py
@@ -1,9 +1,8 @@
 import hashlib
 import itertools
+import string
 
-charset = string.printable
-charset = ''.join([chr(i) for i in range(128)])
-
+charset = string.printable[:-5]
 
 def md5(s):
     return hashlib.md5(s).hexdigest()
@@ -27,6 +26,14 @@ def crack(func):
 def md5crack(x):
     return md5(x) == '03c7c0ace395d80182db07ae2c30f034'
 
+def crack2(x):
+    for k in range(1, len(charset) + 1):
+        for i in itertools.permutations(charset, k):
+            s = ''.join(i)
+            if md5(s)[:4] == x:
+                print s
+                return True
 
 if __name__ == '__main__':
-    print md5crack()
+    while True:
+        crack2(raw_input('>'))
