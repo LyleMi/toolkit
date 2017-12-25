00439393adbeeeb2fc3519ec507bf3a77d6eec88
Lyle <lilyui@126.com>
2017-12-25 08:41:59 +0800

    madd comment for es client

--- dbwrapper/es.py
+++ dbwrapper/es.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+"""Elastic Search Database Client wrapper class
+"""
 
 import elasticsearch
 
 
 class ElasticSearchDB(object):
 
+    """Elastic Search Database Client wrapper
+
+    Attributes:
+        client (obj): ElasticSearchDB client
+        doctype (str): default doc type
+        index (str): default index
+    """
+
     def __init__(self, servers, index, doctype):
+        """init param
+
+        Args:
+            servers (list): Elastic Search Database Server Info
+            index (str): default index
+            doctype (str): default doc type
+        """
         self.client = elasticsearch.Elasticsearch(hosts=es_servers)
         self.index = index
         self.doctype = doctype
 
     def add(self, sid, row):
+        """add some data
+
+        Args:
+            sid (str): data id
+            row (dict): data to be added
+        """
         self.client.index(
             index=self.index,
             doc_type=self.doctype,
@@ -20,13 +43,27 @@ class ElasticSearchDB(object):
         )
 
     def search(self, row):
+        """search data
+
+        Args:
+            row (dict): search definition using the Query DSL 
+
+        Returns:
+            dict: search data
+        """
         return self.client.search(
             index=self.index,
             doc_type=self.doctype,
             body=row
         )
 
-    def update(self, sid, row):
+    def update(self, sid, row_obj):
+        """update data
+
+        Args:
+            sid (str): data id
+            row_obj (dict): data to be added
+        """
         self.client.update(
             index=self.index,
             doc_type=self.doctype,
@@ -34,12 +71,25 @@ class ElasticSearchDB(object):
             id=sid
         )
 
-    def delete(self, sid):
-        self.client.delete(
-            index=self.index,
-            doc_type=self.doctype,
-            id=sid
-        )
+    def delete(self, sid=None, row=None):
+        """delete data, if sid is given, use sid, else use row to search and delete
+
+        Args:
+            sid (str): data id
+            row (dict, optional): search definition using the Query DSL 
+        """
+        if sid is not None:
+            self.client.delete(
+                index=self.index,
+                doc_type=self.doctype,
+                id=sid
+            )
+        elif row is not None:
+            self.client.delete_by_query(
+                index=self.index,
+                doc_type=self.doctype,
+                body=row
+            )
 
 
 if __name__ == '__main__':
