0ae9c932cf5d57d4a83b634b278eb823dc60ae4d
lyle <lilyui@126.com>
2018-04-22 21:42:30 +0800

    add install script for new system

---
+++ misc/install.sh
@@ -0,0 +1,24 @@
+# install some tool for new machine
+
+sudo apt update && sudo apt upgrade
+
+# make tools
+sudo apt install cmake clang
+
+# zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# peda
+git clone https://github.com/longld/peda.git ~/peda
+echo "source ~/peda/peda.py" >> ~/.gdbinit
+
+# pwntools
+sudo apt install python-pip python-dev git libssl-dev libffi-dev build-essential
+sudo pip install --upgrade pip
+sudo pip install --upgrade pwntools
+
+# math
+sudo pip install --upgrade opencv-python
+sudo pip install --upgrade numpy
+sudo pip install --upgrade matplotlib
+sudo pip install --upgrade tensorflow
