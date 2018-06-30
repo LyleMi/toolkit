e879252d5f676491f8770afc2f127fe20904940f
lyle <lilyui@126.com>
2018-06-30 14:01:26 +0800

    add install script for docker compose

--- operations/docker_ubuntu.sh
+++ operations/docker_ubuntu.sh
@@ -24,3 +24,10 @@ sudo add-apt-repository \
 
 sudo apt-get update
 sudo apt-get install docker-ce
+
+# docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# or install docker compose by pip
+sudo pip install docker-compose
