7144877e8dda6815c06e98295d5f20df6a911793
lyle <lilyui@126.com>
2018-07-19 09:01:07 +0800

    update zsh plugin

--- operations/install_debian.sh
+++ operations/install_debian.sh
@@ -18,6 +18,8 @@ sudo pip install --upgrade pip
 
 # zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # tmux
 sudo apt install tmux
