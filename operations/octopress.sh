5369cc52d8a2fd70c4be7db1929ffb652bf14da8
lyle <lilyui@126.com>
2018-09-03 21:56:01 +0800

    add install script for octopress

---
+++ operations/octopress.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update
+sudo apt-get install ruby2.4
+
+git clone git://github.com/imathis/octopress.git octopress
+cd octopress
+gem install bundler
+rbenv rehash    # If you use rbenv, rehash to be able to run the bundle command
+bundle install
+
+rake install
+sudo rake generate
