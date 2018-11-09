44930bf6cf04a461d55fe6b8103a91323861e118
lyle <lilyui@126.com>
2018-11-09 14:45:22 +0800

    add install script for nodejs

---
+++ operations/install_node.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt install nodejs
+
+sudo npm install n -g
+
+# use taobao registry if needed
+sudo npm install n -g --registry=https://registry.npm.taobao.org
+
+# change version
+sudo n 0.12.2
+sudo n stable
+sudo n latest
