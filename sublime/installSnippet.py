a09b6426e5f1f7b85444a34d0e8cc5f011ba2a20
lyle <lilyui@126.com>
2018-11-20 10:58:02 +0800

    add install script for sublime code snippet

---
+++ sublime/installSnippet.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import shutil
+import platform
+
+def main():
+    system = platform.system()
+    if system == 'Windows':
+        sublimeDir = "D:\\Program Files\\Sublime Text 3\\Data\\Packages\\User\\"
+    elif system == 'Darwin':
+        sublimeDir = "~/Library/Application Support/Sublime Text 3/Packages/User/"
+    for snippet in os.listdir("."):
+        if snippet.endswith(".sublime-snippet"):
+            print("install %s" % snippet)
+            shutil.copy(snippet, sublimeDir + snippet)
+
+if __name__ == '__main__':
+    main()
