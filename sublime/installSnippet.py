f169507e97f9d25b7efc40392e250d223f4b1d81
lylemi <lylemi@126.com>
2018-11-20 11:33:48 +0800

    fix bug

--- sublime/installSnippet.py
+++ sublime/installSnippet.py
@@ -11,6 +11,7 @@ def main():
         sublimeDir = "D:\\Program Files\\Sublime Text 3\\Data\\Packages\\User\\"
     elif system == 'Darwin':
         sublimeDir = "~/Library/Application Support/Sublime Text 3/Packages/User/"
+        sublimeDir = os.path.expanduser(sublimeDir)
     for snippet in os.listdir("."):
         if snippet.endswith(".sublime-snippet"):
             print("install %s" % snippet)
