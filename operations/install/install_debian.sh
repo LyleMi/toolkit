0eb6da42a02bc6efa978d01e70e1a0734805760a
lyle <lilyui@126.com>
2018-11-27 09:46:30 +0800

    update personal config for ubuntu

--- operations/install/install_debian.sh
+++ operations/install/install_debian.sh
@@ -25,10 +25,16 @@ sudo pip install --upgrade pip
 # zsh
 # https://github.com/robbyrussell/oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="bira"/' ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
- 
+sed -i 's/^plugins=(/plugins=(\n  zsh-autosuggestions\n  zsh-syntax-highlighting\n  zsh-history-substring-search/' ~/.zshrc
+source ~/.zshrc
+
+# vim
+curl "https://raw.githubusercontent.com/LyleMi/toolkit/master/operations/config/.vimrc" > ~/.vimrc
+
 # tmux
 sudo apt install tmux
 git clone https://github.com/gpakosz/.tmux.git
