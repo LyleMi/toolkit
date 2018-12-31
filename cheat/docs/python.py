92f4ef7b503f022731cc4cc7e04c69b994d396b6
lyle <lilyui@126.com>
2018-12-31 20:58:11 +0800

    update python cheatsheet

--- cheat/docs/python.py
+++ cheat/docs/python.py
@@ -8,15 +8,16 @@ class pythonDoc(BaseDoc):
 
     _doc = {
         "virtual env": """virtualenv venv
-    . venv/bin/activate""",
+. venv/bin/activate""",
         "pip": """pip freeze > requirements.txt
-    pip install -r requirements.txt
-    pip install -U
-    pip install --upgrade
-    pip install --force-reinstall
-    pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xxxx
-    pip install -i https://mirrors.aliyun.com/pypi/simple xxxx
-    """,
-        "jupyter": """sudo jupyter notebook --ip 0.0.0.0 --allow-root
-    """
+pip install -r requirements.txt
+pip install -U
+pip install --upgrade
+pip install --force-reinstall
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple xxxx
+pip install -i https://mirrors.aliyun.com/pypi/simple xxxx""",
+        "jupyter": """sudo jupyter notebook --ip 0.0.0.0 --allow-root""",
+        "upload package": """rm -rf dist
+python setup.py sdist
+python -m twine upload dist/*"""
     }
