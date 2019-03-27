bd346b2337ae06a886da34109fea1607b1787e0b
lyle <lilyui@126.com>
2019-03-27 19:25:34 +0800

    add windows support for change mac

--- misc/changemac.py
+++ misc/changemac.py
@@ -5,16 +5,21 @@ import re
 import os
 import sys
 import random
+import argparse
 import platform
 import subprocess
 
+system = platform.system()
+
 if system == 'Windows':
     import ctypes
-    import _winreg as winreg
+    try:
+        import winreg
+    except ImportError:
+        import _winreg as winreg
     WIN_REGISTRY_PATH = "SYSTEM\\CurrentControlSet\\Control\\Class\\{4D36E972-E325-11CE-BFC1-08002BE10318}"
 
 MACRE = re.compile(r"^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})$")
-system = platform.system()
 
 
 def isRoot():
@@ -35,21 +40,73 @@ def main():
     if not isRoot():
         print("please run this script as root")
         return
-    if len(sys.argv) < 2:
+
+    parser = argparse.ArgumentParser(
+        description='Tool For Change Mac Address',
+        usage='[options]')
+    parser.add_argument('-m', '--mac', metavar='mac',
+                        default='',
+                        help='mac address')
+    parser.add_argument('-i', '--interface', metavar='interface',
+                        default='',
+                        help='network interface')
+    parser.add_argument('-d', '--desc', metavar='desc',
+                        default='',
+                        help='network interface desc')
+    opts = parser.parse_args()
+    if not opts.interface:
+        parser.print_help()
         print("please choose an interface")
         return
-    else:
-        interface = sys.argv[1]
-    if len(sys.argv) < 3:
+    interface = opts.interface
+    desc = opts.desc
+    mac = opts.mac
+    if not mac or not MACRE.match(mac):
         print("choice a random mac")
         mac = ":".join(["%02x" % random.randint(0, 255) for i in range(6)])
-    else:
-        mac = sys.argv[2]
-        if not MACRE.match(mac):
-            print("please use a valid mac address")
-            return
     print("change %s's mac to %s" % (interface, mac))
     if system == 'Windows':
+        reg_hdl = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+        key = winreg.OpenKey(reg_hdl, WIN_REGISTRY_PATH)
+        info = winreg.QueryInfoKey(key)
+
+        adapter_key = None
+        adapter_path = None
+
+        for x in range(info[0]):
+            subkey = winreg.EnumKey(key, x)
+            path = WIN_REGISTRY_PATH + "\\" + subkey
+
+            if subkey == 'Properties':
+                break
+
+            # Check for adapter match for appropriate interface
+            new_key = winreg.OpenKey(reg_hdl, path)
+            try:
+                adapterDesc = winreg.QueryValueEx(new_key, "DriverDesc")
+                if adapterDesc[0] == desc:
+                    adapter_path = path
+                    break
+                else:
+                    winreg.CloseKey(new_key)
+            except WindowsError as err:
+                if err.errno == 2:  # register value not found, ok to ignore
+                    pass
+                else:
+                    raise err
+
+        if adapter_path is None:
+            print('get adapter path fail')
+            winreg.CloseKey(key)
+            winreg.CloseKey(reg_hdl)
+            return
+        else:
+            print(f'adapter_path {adapter_path}')
+        adapter_key = winreg.OpenKey(reg_hdl, adapter_path, 0, winreg.KEY_WRITE)
+        winreg.SetValueEx(adapter_key, "NetworkAddress", 0, winreg.REG_SZ, mac)
+        winreg.CloseKey(adapter_key)
+        winreg.CloseKey(key)
+        winreg.CloseKey(reg_hdl)
         restartDevice(interface)
     elif system == 'Darwin':
         os.system("sudo ifconfig %s ether %s" % (interface, mac))
